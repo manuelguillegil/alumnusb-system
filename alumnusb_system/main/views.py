2246a48a3015572fbdd38de55d82c9ba0623095d
Manuel Guillermo Gil <manuelguillermogil@gmail.com>
2020-02-03 21:15:34 -0400

    Se configuró la carpeta de templates del proyecto

--- alumnusb_system/main/views.py
+++ alumnusb_system/main/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    return render(request,'home.html',{'variable':''})
